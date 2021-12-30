e06840651c4407d74c75902b5e8b1e657609baa5
Shub61 <subhamgupta84@gmail.com>
2021-12-30 07:55:46 +0530

    Commit-2

--- functional_interface/src/func_interface/DefaultMethodDiamondProblem.java
+++ functional_interface/src/func_interface/DefaultMethodDiamondProblem.java
@@ -1,2 +1,43 @@
-package func_interface;public class DefaultMethodDiamondProblem {
+package func_interface;
+
+interface RBCBank{
+    boolean depositMoneyToRBC(int amt);
+    default void printAmount(){
+        System.out.println("PRINT from RBC");
+    }
+    void checkAmount();
+}
+
+interface CIBCBank{
+    boolean depositMoneyToCIBC(int amt);
+    default void printAmount(){
+        System.out.println("PRINT from CIBC");
+    }
+    void checkAmount();
+}
+public class DefaultMethodDiamondProblem implements RBCBank, CIBCBank{
+    @Override
+    public boolean depositMoneyToRBC(int amt) {
+        return false;
+    }
+
+    @Override
+    public void printAmount() {
+        RBCBank.super.printAmount();
+    }
+
+    @Override
+    public boolean depositMoneyToCIBC(int amt) {
+        return false;
+    }
+
+    @Override
+    public void checkAmount(){
+        System.out.println("Yes Amount is present");
+    }
+
+    public static void main(String[] args) {
+        DefaultMethodDiamondProblem defaultMethodDiamondProblem = new DefaultMethodDiamondProblem();
+        defaultMethodDiamondProblem.printAmount();
+    }
 }
--- functional_interface/src/func_interface/LambdaExpressionDemo.java
+++ functional_interface/src/func_interface/LambdaExpressionDemo.java
@@ -1,2 +1,13 @@
-package func_interface;public class LambdaExpressionDemo {
+package func_interface;
+
+import java.util.Optional;
+
+interface LambdaExpression{
+    int getLength(String s);
+}
+public class LambdaExpressionDemo {
+    public static void main(String[] args) {
+        LambdaExpression lambdaExpression = (s) -> s.length();
+        System.out.println(lambdaExpression.getLength("Shubham"));
+    }
 }
